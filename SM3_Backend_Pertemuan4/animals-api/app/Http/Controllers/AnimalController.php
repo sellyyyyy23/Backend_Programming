eb9fb3b12bb967627f2329bc0805e1bc5f86e4e7
sellyyyyy23 <seli.mulyani@dac-solution.com>
2021-10-29 21:40:32 +0700

    UPDATE tugas pekan 4

--- SM3_Backend_Pertemuan4/animals-api/app/Http/Controllers/AnimalController.php
+++ SM3_Backend_Pertemuan4/animals-api/app/Http/Controllers/AnimalController.php
@@ -22,41 +22,27 @@ class AnimalController extends Controller
     ];
 
     function index(){
-        // echo "ini adalah data animals";
-        // return ([
-        //     "name" => "cat",
-        //     "warna" => "merah",
-        // ]);
+		echo "Menampilkan Data Hewan: <br>  <br> ";
         foreach ($this->animals as $animal) {
-			echo "Nama Hewan: $animal[name]  <br>";
+			echo "$animal[name]  <br>";
 		}
     }
 
     function create(Request $request){
-        //echo "anda berhasil menambahkan data animals baru";
         array_push($this->animals, $request);
 		$this->index();
     }
 
     function update(Request $request, $id){
-        // $name = $request->nama;
-        // $collor = $request->warna;
-
-    
-        // echo "anda baru saja memperbaruhi data animal dari id $id";
-        // echo "<br>";
-        // echo "nama terbaru adalah $name";
-        // echo "<br>";
-        // echo "dengan warna $collor";
-        echo 'Update animals '.$this->animals[$id]['name'].' menjadi : animals '.$request->name.'<br>';
-        echo "<br>";
-		$this->animals[$id] = $request;
-		$this->index();
+        
+		echo 'Update Animals '.$this->animals[$id]['name'].' menjadi : animals '.$request->name.'<br><br>';
+		$this->animals[$id]['name'] = $request->name;
+        return $this->index();
     }
 
     function destroy($id){
         //echo "anda baru saja mengapus data animal dari id $id";
-        echo "Menghapus data animals id: $id <br>";
+        echo "Menghapus data animals ID: $id <br><br>";
 		array_splice($this->animals, $id, 1);
 		$this->index();
     }
